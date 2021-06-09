7bd393aacb894733761d7b31b326bfcab3188001
Igor Gayday <gaidai92@gmail.com>
2021-06-09 10:47:09 -0500

    Adds support for signed numbers

--- qde.py
+++ qde.py
@@ -65,7 +65,7 @@ def build_discretization_vector(qbits_integer, qbits_decimal):
     return np.array([2 ** -j for j in j_range])
 
 
-def build_qubo_matrix(f, dx, y1, H_discret_elem, d_discret_elem):
+def build_qubo_matrix(f, dx, y1, H_discret_elem, d_discret_elem, signed):
     """Builds a QUBO matrix (Q) corresponding to a given differential equation. 
 
     A sample solution is represented by its values at grid points discretized using fixed point representation with set number of qubits.
@@ -78,19 +78,23 @@ def build_qubo_matrix(f, dx, y1, H_discret_elem, d_discret_elem):
         y1 (float): Solution's value at the leftmost point (boundary condition).
         H_discret_elem (numpy.ndarray (2D)): Matrix discretization element for given number of qubits (see also build_discretization_matrix).
         d_discret_elem (numpy.ndarray (1D)): Vector discretization element for given number of qubits (see also build_discretization_vector).
+        signed (bool): Whether to use signed or unsigned number representation. Using signed numbers shifts representation range from 0..L to -L/2..L/2, where L = 2 ** qbits_integer.
 
     Returns:
         Q (numpy.ndarray): QUBO matrix.
     """
     H_cont = build_quadratic_minimization_matrix(len(f), dx)
     d_cont = build_quadratic_minimization_vector(f, dx, y1)
+    if signed:
+        d_cont[0] -= 2 * d_discret_elem[0] / dx ** 2
+
     H_bin = np.block([[H_discret_elem * val for val in row] for row in H_cont])
     d_bin = np.block([d_discret_elem * val for val in d_cont])
     Q = H_bin + np.diag(d_bin)
     return Q
 
 
-def solve(f, dx, y1, qbits_integer, qbits_decimal, points_per_qubo=1, **kwargs):
+def solve(f, dx, y1, qbits_integer, qbits_decimal, signed=True, points_per_qubo=1, average_solutions=False, **kwargs):
     """Solves a given differential equation, defined by f and y1, by formulating it as a QUBO problem with given discretization precision.
 
     Args:
@@ -99,7 +103,9 @@ def solve(f, dx, y1, qbits_integer, qbits_decimal, points_per_qubo=1, **kwargs):
         y1 (float): Solution's value at the leftmost point (boundary condition).
         qbits_integer (int): Number of qubits to represent integer part of each expansion coefficient (value) of the sample solution.
         qbits_decimal (int): Number of qubits to represent decimal part of each expansion coefficient.
+        signed (bool): Whether to use signed or unsigned number representation. Using signed numbers shifts representation range from 0..L to -L/2..L/2, where L = 2 ** qbits_integer.
         points_per_qubo (int): Number of points to propagate in each QUBO. Last point from the previous solution is used as boundary condition for the next solution.
+        average_solutions (bool): If true, all found solutions are averaged according to number of times they were found. If false, only the best solution is considered.
         kwargs (dict): Additional keyword arguments to QBSolv().sample_qubo
 
     Returns:
@@ -110,19 +116,34 @@ def solve(f, dx, y1, qbits_integer, qbits_decimal, points_per_qubo=1, **kwargs):
 
     H_discret_elem = build_discretization_matrix(qbits_integer, qbits_decimal)
     d_discret_elem = build_discretization_vector(qbits_integer, qbits_decimal)
+    error = 0
     for i in range(1, len(f), points_per_qubo):
         y1 = solution[i - 1]
         next_i = min(i + points_per_qubo, len(f))
-        Q = build_qubo_matrix(f[i - 1 : next_i], dx, y1, H_discret_elem, d_discret_elem)
+        next_f = f[i - 1 : next_i]
+        Q = build_qubo_matrix(next_f, dx, y1, H_discret_elem, d_discret_elem, signed)
         sample_set = QBSolv().sample_qubo(Q, **kwargs)
         samples_bin = np.array([list(sample.values()) for sample in sample_set])
         samples_bin_structured = samples_bin.reshape(samples_bin.shape[0], -1, len(d_discret_elem))
+
         samples_cont = np.sum(samples_bin_structured * d_discret_elem, 2)
-        solution[i:next_i] = samples_cont[0, :]
+        error_shift = (y1 ** 2 + 2 * y1 * next_f[0] * dx) / dx ** 2 + np.sum(next_f[0:-1] ** 2)
+        if signed:
+            samples_cont -= 2 ** (qbits_integer - 1)
+            error_shift += (4 ** (qbits_integer - 1) + 2 ** qbits_integer * (y1 + next_f[0] * dx)) / dx ** 2
 
-        #  error_shift = (y1 / dx) ** 2 + 2 * f[0] * y1 / dx + np.sum(f[0:-1] ** 2)
-        #  errors = np.array([sample.energy for sample in sample_set.data(fields=['energy'])]) + error_shift
+        if average_solutions:
+            num_occurrences = np.array([sample.num_occurrences for sample in sample_set.data()])
+            weights = num_occurrences / np.sum(num_occurrences)
+            solution[i:next_i] = np.sum(samples_cont * weights[:, np.newaxis], 0)
+            all_energies = np.array([sample.energy for sample in sample_set.data()])
+            solution_energy = np.sum(all_energies * weights)
+        else:
+            solution[i:next_i] = samples_cont[0, :]
+            solution_energy = next(sample_set.data()).energy
 
-    return solution
+        error += solution_energy + error_shift
+
+    return solution, error
 
 
