46442183bf47100832259fbaacd931a56f92ff4a
Igor Gayday <gaidai92@gmail.com>
2021-06-09 10:48:05 -0500

    Refactors, extra drawing functions

--- qde_test.py
+++ qde_test.py
@@ -1,3 +1,4 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import qde
@@ -5,7 +6,7 @@ from qpsolvers import solve_qp
 
 import addcopyfighandler
 
-def test_exp_qp():
+def test_qp():
     """Solves dy/dx = exp(x) by formulating it as a Quadratic Programming problem (in real coefficients)."""
     grid_from = 0
     grid_to = 0.1
@@ -29,14 +30,15 @@ def test_qbsolv():
     print(res)
 
 
-def test_exp_qubo(grid_from = 0, grid_to = 1, N = 11, y1 = 1, qbits_integer = 2, qbits_decimal = 30, points_per_qubo = 1, num_repeats = 200):
+def test_qubo(grid_from=0, grid_to=1, N=11, y1=1, qbits_integer=3, qbits_decimal=30, num_repeats=200, **kwargs):
     """Solves dy/dx = exp(x) by formulating it as a QUBO problem (in binary coefficients)."""
     grid = np.linspace(grid_from, grid_to, N)
     f = np.exp(grid)
     dx = grid[1] - grid[0]
-    solution = qde.solve(f, dx, y1, qbits_integer, qbits_decimal, points_per_qubo, num_repeats=num_repeats)
+    solution, error = qde.solve(f, dx, y1, qbits_integer, qbits_decimal, num_repeats=num_repeats, **kwargs)
     print('Solution:')
     print(solution)
+    print(f'Error: {error}')
 
 
 def test_matplotlib():
@@ -45,17 +47,43 @@ def test_matplotlib():
     plt.show()
 
 
-def plot_solution_error(points_per_qubo = 1):
-    """Plots solution error as a function of number of grid points."""
-    grid_from = 0
-    grid_to = 1
-    Ns = np.geomspace(10, 100, 5, dtype=int)
-    y1 = 1
-    qbits_integer = 2
-    qbits_decimal = 30
-    num_repeats = 200
+def apply_plot_settings(axes):
+    """Applies common settings."""
+    axes.autoscale()
+    axes.set_xscale('log')
+    axes.set_yscale('log')
+    axes.set_xlabel('N')
+    axes.set_ylabel('Error')
 
-    plot_data = np.empty((2, 2, len(Ns)))
+
+def get_axes(axes):
+    """If None, creates a new plot, otherwise returns its argument."""
+    if axes is None:
+        _, axes = plt.subplots()
+        apply_plot_settings(axes)
+    return axes
+
+
+def filter_kwargs(func, kwargs):
+    """Returns kwargs subset where only keys recognized by func are left."""
+    return {key: value for key, value in kwargs.items() if key in func.__code__.co_varnames}
+
+
+def get_plot_keys():
+    """Returns known plot keys."""
+    return {'color', 'linestyle', 'linewidth',  'marker', 'markersize', 'label'}
+
+
+def filter_kwargs_plot(kwargs):
+    """Filters out keys unknown to plot function."""
+    known_keys = get_plot_keys()
+    return {key: value for key, value in kwargs.items() if key in known_keys}
+
+
+def plot_qp_error(grid_from=0, grid_to=1, Ns=np.geomspace(10, 100, 5, dtype=int), y1=1, axes=None):
+    """Plots QP solution error as a function of number of grid points."""
+    axes = get_axes(axes)
+    plot_data = np.empty((2, len(Ns)))
     for i in range(len(Ns)):
         N = Ns[i]
         grid = np.linspace(grid_from, grid_to, N)
@@ -66,23 +94,33 @@ def plot_solution_error(points_per_qubo = 1):
         d = qde.build_quadratic_minimization_vector(f, dx, y1)
         solution_real = solve_qp(H, d)
         error = abs(solution_real[-1] - np.exp(grid_to))
-        plot_data[0, :, i] = (N, error)
+        plot_data[:, i] = (N, error)
 
-        solution_bin = qde.solve(f, dx, y1, qbits_integer, qbits_decimal, points_per_qubo, num_repeats=num_repeats)
+    axes.plot(plot_data[0, :], plot_data[1, :], 'b.-', markersize=10, label='QP')
+    return axes
+
+
+def plot_qubo_error(grid_from=0, grid_to=1, Ns=np.geomspace(10, 100, 5, dtype=int), y1=1, qbits_integer=3, qbits_decimal=30, num_repeats=200, axes=None, **kwargs):
+    """Plots QUBO error as a function of number of grid points."""
+    axes = get_axes(axes)
+    plot_data = np.empty((2, len(Ns)))
+    for i in range(len(Ns)):
+        N = Ns[i]
+        grid = np.linspace(grid_from, grid_to, N)
+        f = np.exp(grid)
+        dx = grid[1] - grid[0]
+
+        solution_bin, error_sln = qde.solve(f, dx, y1, qbits_integer, qbits_decimal, num_repeats=num_repeats, **kwargs)
+        print(error_sln)
         error = abs(solution_bin[-1] - np.exp(grid_to))
-        plot_data[1, :, i] = (N, error)
+        plot_data[:, i] = (N, error)
 
-    plt.plot(plot_data[0, 0, :], plot_data[0, 1, :], 'b.-', markersize=10)
-    plt.plot(plot_data[1, 0, :], plot_data[1, 1, :], 'r.--', markersize=10)
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.xlabel('N')
-    plt.ylabel('Error')
-    plt.show()
+    axes.plot(plot_data[0, :], plot_data[1, :], markersize=10, **filter_kwargs_plot(kwargs))
+    return axes
 
 
 if __name__ == '__main__':
     np.set_printoptions(precision=15, linewidth=200)
     plot_solution_error()
-    #  test_exp_qp()
-    #  test_exp_qubo()
+    if not mpl.is_interactive():
+        plt.show()
